262390ba70d2ba61a552f6def74f736280212010
Ethan Grebmeier <ethangrebmeier@gmail.com>
2025-10-20 16:05:13 -0700

    feat: icons for categories

--- components/category-tag.tsx
+++ components/category-tag.tsx
@@ -1,3 +1,6 @@
+import { categoryOptions } from '../features/shared/category/categories';
+import { Icon } from './ui/icon';
+import { Pill } from './ui/pill';
 import { Text } from './ui/text';
 
 type CategoryTagProps = {
@@ -5,9 +8,20 @@ type CategoryTagProps = {
 };
 
 export const CategoryTag = ({ category }: CategoryTagProps) => {
+  const categoryOption = categoryOptions.find(opt => opt.value === category);
+  if (!categoryOption) {
+    return null;
+  }
+  const { icon } = categoryOption.style;
   return (
-    <Text className="rounded-full bg-muted px-2 py-1 text-sm text-muted-foreground">
-      {category}
-    </Text>
+    <Pill
+      className="border-yellow-500 bg-yellow-100"
+      icon={<Icon as={icon} size={16} />}
+      hasValue={true}
+    >
+      <Text className="text-sm font-medium text-foreground">
+        {categoryOption.label}
+      </Text>
+    </Pill>
   );
 };
