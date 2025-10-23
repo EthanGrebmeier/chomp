2a50f8b47c80ad65750554198ae467c4b2cbec21
Ethan Grebmeier <ethangrebmeier@gmail.com>
2025-10-22 19:59:05 -0700

    fix: icon blocking

--- components/ui/icon.tsx
+++ components/ui/icon.tsx
@@ -40,9 +40,15 @@ cssInterop(IconImpl, {
  * @param {number} size - Icon size (defaults to 14).
  * @param {...LucideProps} ...props - Additional Lucide icon props passed to the "as" icon.
  */
-function Icon({ as: IconComponent, className, size = 14, ...props }: IconProps) {
+function Icon({
+  as: IconComponent,
+  className,
+  size = 14,
+  ...props
+}: IconProps) {
   return (
     <IconImpl
+      pointerEvents="none"
       as={IconComponent}
       className={cn('text-foreground', className)}
       size={size}
