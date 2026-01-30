5e5228d3254fbd5e5dfe9754f43e151eefc16599
Ethan Grebmeier <ethangrebmeier@gmail.com>
2026-01-29 21:03:28 -0800

    feat: properly colored tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
 import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
+import { DynamicColorIOS } from 'react-native';
 
 import { useGroceryLists } from '@/features/grocery-lists/instant/useGroceryLists';
+import { useTheme } from '@/hooks/use-theme';
 import { InstantAuthHandler } from '@/lib/instant/use-clerk-auth';
 
 import { useRecipes } from '../../features/recipes/hooks';
@@ -10,24 +12,32 @@ export default function Layout() {
   useRecipes();
   useGroceryLists();
 
+  const theme = useTheme();
   return (
     <>
       <InstantAuthHandler />
-      <NativeTabs>
+      <NativeTabs
+        tintColor={DynamicColorIOS({
+          dark: theme.primary,
+          light: theme.primary,
+        })}
+      >
         <NativeTabs.Trigger name="index">
-          <Icon sf="square.and.pencil" />
+          {/* @ts-ignore This is a real symbol  */}
+          <Icon sf={{ default: 'receipt', selected: 'receipt.fill' }} />
           <Label> List </Label>
         </NativeTabs.Trigger>
         <NativeTabs.Trigger name="plans">
-          <Icon sf="calendar" />
+          {/* @ts-ignore This is a real symbol  */}
+          <Icon sf={{ default: 'calendar', selected: 'calendar.fill' }} />
           <Label> Meal Plan </Label>
         </NativeTabs.Trigger>
         <NativeTabs.Trigger name="recipes">
-          <Icon sf="book.fill" />
+          <Icon sf={{ default: 'book', selected: 'book.fill' }} />
           <Label> Recipes </Label>
         </NativeTabs.Trigger>
         <NativeTabs.Trigger name="settings">
-          <Icon sf="gearshape.fill" />
+          <Icon sf={{ default: 'gearshape', selected: 'gearshape.fill' }} />
           <Label> Settings </Label>
         </NativeTabs.Trigger>
       </NativeTabs>
